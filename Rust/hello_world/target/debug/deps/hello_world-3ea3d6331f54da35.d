36702ebd44651a485a9502590437c3c0803f9e39
Rogue Blue <rogueblueprint@protonmail.com>
2022-12-12 12:43:05 -0700

    Deleted hello world program

--- Rust/hello_world/target/debug/deps/hello_world-3ea3d6331f54da35.d
+++
@@ -1,5 +0,0 @@
-/home/sivnerof/Code/Try-Hack-Me/Rust/hello_world/target/debug/deps/hello_world-3ea3d6331f54da35: src/main.rs
-
-/home/sivnerof/Code/Try-Hack-Me/Rust/hello_world/target/debug/deps/hello_world-3ea3d6331f54da35.d: src/main.rs
-
-src/main.rs:
