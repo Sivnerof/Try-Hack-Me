e5efab09ed5fef09eb0a14c30c1426a0c816460b
Rogue Blue <rogueblueprint@protonmail.com>
2022-12-12 12:41:34 -0700

    Undo

---
+++ Rust/hello_world/target/debug/deps/hello_world-3ea3d6331f54da35.d
@@ -0,0 +1,5 @@
+/home/sivnerof/Code/Try-Hack-Me/Rust/hello_world/target/debug/deps/hello_world-3ea3d6331f54da35: src/main.rs
+
+/home/sivnerof/Code/Try-Hack-Me/Rust/hello_world/target/debug/deps/hello_world-3ea3d6331f54da35.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/sivnerof/Code/Try-Hack-Me/Rust/hello_world/target/debug/hello_world: /home/sivnerof/Code/Try-Hack-Me/Rust/hello_world/src/main.rs
