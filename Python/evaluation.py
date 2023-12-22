56e3e84b85639849ce71c6932e9101ce0c03ee96
Rogue Blue <rogueblueprint@protonmail.com>
2023-12-21 21:42:13 -0700

    Added evaluation exercise program

---
+++ Python/evaluation.py
@@ -0,0 +1,21 @@
+print("Evaluating string expressions...")
+
+# What does each string expression evaluate to?
+print("python" + "3")
+print("computer" + " " + "programming")
+print("\n\n\n")
+print("4" + "3.1" + "5.8")
+
+# Display a divider surrounded by blank lines.
+print()
+print("----------")
+print()
+
+print("Evaluating integer and float expressions...")
+
+# What does each numeric expression evaluate to?
+print(40 + 7)
+print(1.5 + 1.5)
+print(2.00000)
+print(-1 + 0.66)
+
