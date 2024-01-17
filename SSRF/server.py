2d376010601bcc0a6eb407df55f38bdb3195bbb4
Rogue Blue <rogueblueprint@protonmail.com>
2024-01-17 14:09:22 -0700

    Added ssrf room server program

---
+++ SSRF/server.py
@@ -0,0 +1,34 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+from urllib.parse import unquote
+class CustomRequestHandler(SimpleHTTPRequestHandler):
+
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')  # Allow requests from any origin
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        super().end_headers()
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, GET request!')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
+
+        self.send_response(200)
+        self.end_headers()
+
+        # Log the POST data to data.html
+        with open('data.html', 'a') as file:
+            file.write(post_data + '\n')
+        response = f'THM, POST request! Received data: {post_data}'
+        self.wfile.write(response.encode('utf-8'))
+
+if __name__ == '__main__':
+    server_address = ('', 8080)
+    httpd = HTTPServer(server_address, CustomRequestHandler)
+    print('Server running on http://localhost:8080/')
+    httpd.serve_forever()
+
